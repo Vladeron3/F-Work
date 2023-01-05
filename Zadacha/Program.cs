3e62ba67d1da5b6b6ecb9f4ad8b77b8e5e001b86
VladimirEronin <vl.eronin@mail.ru>
2023-01-05 15:15:10 +0300

    реализовано решение задачи в папке Zadacha

--- Zadacha/Program.cs
+++ Zadacha/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача. Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+//длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+//либо задать н астарте выполнения алгоритма. При решении не реокмендуется пользоваться коллекциями, 
+//лучше обойтись исключительно массивами.
+void Zadacha()
+{
+    string[] array = { "hello", "2", "world", ":-)" };
+    string[] newArrey = new string[array.Length];
+    Console.WriteLine($"Исходный массив:");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + $" ");
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+
+    Console.WriteLine("Новый массив:");
+
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArrey[i] = array[i]; ;
+            Console.Write(" " + newArrey[i]);
+        }
+
+    }
+    Console.WriteLine();
+}
+Zadacha();
+
+
